f78f93b2882e89303e34b668155bbfdaaccf6bb1
wildRaccoon <chukvm@gmail.com>
2019-04-05 20:49:05 +0300

    add init of test

---
+++ src/sp.auth.test/app/commands/account/CreateAccountTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace sp.auth.test.app.commands.account
+{
+    [TestFixture]
+    public class CreateAccountTest
+    {
+        public
+    }
+}
