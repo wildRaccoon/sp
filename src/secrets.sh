902a56aae90e3f534ff90a58ca67c0cf6056a8f0
wildRaccoon <chukvm@gmail.com>
2019-04-14 19:06:05 +0300

    fix configration

--- src/secrets.sh
+++ src/secrets.sh
@@ -2,8 +2,8 @@ COUNT=$(dotnet user-secrets list --id Development -p ./sp.sln | grep sp.auth:has
 
 if [ "$COUNT" = "0" ]
 then 
-    dotnet user-secrets set "sp.auth:hash:salt" "my_prod_salt" --id Development -p ./sp.sln
-    dotnet user-secrets set "sp.auth:hash:secret" "my_prod_secret" --id Development -p ./sp.sln
+    dotnet user-secrets set "sp.auth:hash:salt" "9aa3500b-dd8b-41f0-a51a-ac351f1f29df" --id Development -p ./sp.sln
+    dotnet user-secrets set "sp.auth:hash:secret" "b6767801-5e19-4af8-b889-d09d07c560d2" --id Development -p ./sp.sln
 else
     dotnet user-secrets list --id Development -p ./sp.sln
 fi
